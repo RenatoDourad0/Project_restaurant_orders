4c06803f8d84557733f371dd700eada2a1d6bdc5
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-05-26 08:47:12 -0300

    feat: test for ingredient and rstriction classes

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,25 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+)  # noqa: F401, E261, E501
+import pytest
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ing = Ingredient("farinha")
+    sameIng = Ingredient("farinha")
+    othering = Ingredient("bacon")
+
+    assert ing.name == "farinha"
+    assert ing.__eq__(sameIng)
+
+    assert ing.__repr__() == "Ingredient('farinha')"
+    assert ing.__hash__() == sameIng.__hash__()
+
+    assert ing.restrictions.__eq__(Ingredient("farinha").restrictions)
+
+    assert ing.__hash__() == othering.__hash__()
+
+    with pytest.raises(AssertionError):
+        assert ing.__hash__() == othering.__hash__()
+        assert ing.__eq__(othering)
