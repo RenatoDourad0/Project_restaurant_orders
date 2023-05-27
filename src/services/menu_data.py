57cee7567789994f95d8c11d0fa01c343aed5f83
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-05-27 10:11:39 -0300

    feat: req 3. mapeamentopratos - ingredientes

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,20 @@
 # Req 3
+from src.models.dish import Ingredient, Dish
+from csv import DictReader
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+
+        with open(source_path) as menu:
+            data = DictReader(menu)
+            for item in data:
+                dish = Dish(item["dish"], float(item["price"]))
+
+                if dish not in menu:
+                    menu[dish] = dish
+
+                menu[dish].add_ingredient_dependency(
+                    Ingredient(item["ingredient"]), int(item["recipe_amount"])
+                )
